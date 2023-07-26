9fad1fd7be5da5d7f30446482ab42990d7af7637
LucasRebasa <lrebasa@baufest.com>
2023-07-26 17:24:47 -0300

    Se agrega funcionalidad a las flechas del Carousel

--- src/components/Carrousel.tsx
+++ src/components/Carrousel.tsx
@@ -15,15 +15,28 @@ const Carrousel: React.FC = (props) => {
   ];
   const [currentItem, setCurrentItem] = useState(0);
 
-  function handleNextImage(): void {}
+  function handleNextImage(): void {
+    setCurrentItem((current) =>
+      current === items.length - 1 ? 0 : current + 1
+    );
+  }
+
+  function handlePrevImage(): void {
+    setCurrentItem((current) =>
+      current === 0 ? items.length - 1 : current - 1
+    );
+  }
 
   return (
-    // TODO mostrar una sola imagen por vez 
-    <section className="overflow-hidden bg-slate-50/50">
-      <div className="flex">
+    // TODO mostrar una sola imagen por vez
+    <section className="overflow-hidden relative bg-slate-50/50">
+      <div
+        className="flex transition-transform ease-out duration-500"
+        style={{ transform: `translateX(-${currentItem * 100}%)` }}
+      >
         {items.map((e, index) => {
           return (
-            <div key={index} className='basis-full flex-shrink-0 text-center'>
+            <div key={index} className="basis-full flex-shrink-0 text-center">
               <h1>asdfs</h1>
               <h1>asdf</h1>
               <h1>asdf</h1>
@@ -32,12 +45,12 @@ const Carrousel: React.FC = (props) => {
           );
         })}
       </div>
-      <div className="absolute">
-        <button className="">
-          <i className="fa-solid fa-arrow-right"></i>
+      <div className="flex items-center justify-between absolute inset-0 m-4">
+        <button onClick={handlePrevImage}>
+          <i className="fa-solid fa-arrow-left text-2xl"></i>
         </button>
-        <button className="">
-          <i className="fa-solid fa-arrow-left"></i>
+        <button onClick={handleNextImage}>
+          <i className="fa-solid fa-arrow-right text-2xl"></i>
         </button>
       </div>
     </section>
